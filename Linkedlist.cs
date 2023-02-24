0b17908d856e82663e4bb2bd6731b2f7ad4adf06
arivusurya <arivusurya.3@gmail.com>
2023-02-24 21:44:43 +0530

    Adding the value to the Linked List

---
+++ Linkedlist.cs
@@ -0,0 +1,30 @@
+using System;
+public class Node{
+        public int data;
+        public Node next;
+
+        public Node(int data){
+            this.data = data;
+        }
+}
+
+public class LinkedList{
+         public Node head;
+        private int Count ;
+    public void Add(int data){
+            Node node = new Node(data);
+            Count++;
+
+            if(head == null){
+                head = node;
+            }else{
+                Node temp = this.head;
+
+                while(temp.next !=null){
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is created",node.data);
+        }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructure{
+
+    public class Program{
+        public static void Main(string[] args)
+        {
+            LinkedList list  = new LinkedList();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+        }
+
+    }
+
+
+
+}
